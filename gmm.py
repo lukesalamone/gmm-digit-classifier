83c33f04964f04c88c34e91e67cc1af3a70aa97b
lukesalamone <lukesalamone94@gmail.com>
2020-12-20 13:48:39 -0600

    added classifiers

---
+++ gmm.py
@@ -0,0 +1,101 @@
+import numpy as np
+from kmeans import KMeans
+from scipy.stats import multivariate_normal
+
+class GMM():
+    def __init__(self, n_clusters, covariance_type):
+        self.n_clusters = n_clusters
+        allowed_covariance_types = ['spherical', 'diagonal']
+        if covariance_type not in allowed_covariance_types:
+            raise ValueError(f'covariance_type must be in {allowed_covariance_types}')
+        self.covariance_type = covariance_type
+
+        self.means = None
+        self.covariances = None
+        self.mixing_weights = None
+        self.max_iterations = 200
+
+    def fit(self, features):
+        kmeans = KMeans(self.n_clusters)
+        kmeans.fit(features)
+        self.means = kmeans.means
+        self.covariances = self._init_covariance(features.shape[-1])
+        self.mixing_weights = np.random.rand(self.n_clusters)
+        self.mixing_weights /= np.sum(self.mixing_weights)
+        prev_log_likelihood = -float('inf')
+        log_likelihood = self._overall_log_likelihood(features)
+
+        n_iter = 0
+        rounds = 0
+        while log_likelihood - prev_log_likelihood > 1e-4 and n_iter < self.max_iterations:
+            print('round: ', rounds)
+            rounds += 1
+
+            prev_log_likelihood = log_likelihood
+
+            assignments = self._e_step(features)
+            self.means, self.covariances, self.mixing_weights = (
+                self._m_step(features, assignments)
+            )
+
+            log_likelihood = self._overall_log_likelihood(features)
+            n_iter += 1
+
+    def predict(self, features):
+        posteriors = self._e_step(features)
+        return np.argmax(posteriors, axis=-1)
+
+    # expectation step
+    def _e_step(self, features):
+        return np.array([self._posterior(features, k)[0] for k in range(self.n_clusters)]).transpose()
+
+    # maximization step
+    def _m_step(self, features, assignments):
+        mixing_weights = []
+        means = []
+        covariances = []
+        for cluster in range(self.n_clusters):
+            gammas = assignments[:,cluster]
+            r = np.sum(gammas)
+
+            # calculate weights
+            w = r / len(features)
+            mixing_weights.append(w)
+
+            # calculate means
+            mu = (gammas @ features) / r
+            means.append(mu)
+
+            # calculate covariances
+            diff_sq = (features-mu) ** 2
+            cov = (gammas @ diff_sq) / r
+            covariances.append(cov)
+
+        return np.array(means), np.array(covariances), np.array(mixing_weights)
+
+    def _init_covariance(self, n_features):
+        if self.covariance_type == 'spherical':
+            return np.random.rand(self.n_clusters)
+        elif self.covariance_type == 'diagonal':
+            return np.random.rand(self.n_clusters, n_features)
+
+    def _log_likelihood(self, features, k_idx):
+        return np.array([np.log(self.mixing_weights[k_idx]) + p for p in multivariate_normal.logpdf(features, self.means[k_idx], self.covariances[k_idx])])
+
+    def _overall_log_likelihood(self, features):
+        denom = [
+            self._log_likelihood(features, j) for j in range(self.n_clusters)
+        ]
+        return np.sum(denom)
+
+    def _posterior(self, features, k):
+        num = self._log_likelihood(features, k)
+        denom = np.array([
+            self._log_likelihood(features, j)
+            for j in range(self.n_clusters)
+        ])
+
+        max_value = denom.max(axis=0, keepdims=True)
+        denom_sum = max_value + np.log(np.sum(np.exp(denom - max_value), axis=0))
+        posteriors = np.exp(num - denom_sum)
+        return posteriors
---
+++ kmeans.py
@@ -0,0 +1,63 @@
+import numpy as np
+import numpy.random as rand
+
+INFINITY = float('inf')
+
+def distance(a, b):
+    return np.linalg.norm(a-b)
+
+class KMeans:
+    def __init__(self, n_clusters):
+        self.n_clusters = n_clusters
+        self.means = []
+
+    def fit(self, features):
+        features = np.apply_along_axis(lambda f: {'feature': f, 'cluster': -1}, 1, features)
+        self.means = list(map(lambda x: features[x]['feature'], rand.choice(len(features), self.n_clusters, False)))
+
+        updated = True
+        # rounds = 0
+        while updated:
+            # print('round: ', rounds)
+            # rounds += 1
+
+            updated, features = self.update_assignments(features)
+            if not updated:
+                return
+
+            self.update_means(features)
+
+    def predict(self, features):
+        return np.apply_along_axis(lambda feature: self.closest_cluster_index(feature), 1, features)
+
+    def update_assignments(self, features):
+        updated = False
+
+        for f in features:
+            i = self.closest_cluster_index(f['feature'])
+            if i != f['cluster']:
+                updated = True
+                f['cluster'] = i
+
+        return updated, features
+
+    def closest_cluster_index(self, feature):
+        min_distance = INFINITY
+        min_index = 0
+
+        for i in range(self.n_clusters):
+            d = distance(feature, self.means[i])
+            if d < min_distance:
+                min_distance = d
+                min_index = i
+
+        return min_index
+
+    # update means to be averages of each feature in that cluster
+    def update_means(self, features):
+        means = [[] for i in range(self.n_clusters)]
+
+        for f in features:
+            means[f['cluster']].append(f['feature'])
+
+        self.means = list(map(lambda cluster: np.mean(cluster, 0), means))
